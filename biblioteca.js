805018a30ca682b64ed72d1e332c34cb9964a487
SergioAriel <solis.sergioariel@gmail.com>
2020-01-22 12:48:42 -0300

    Folder

---
+++ biblioteca.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var showChar = 300;
+    var ellipsestext = "...";
+    var moretext = "Seguir leyendo >";
+    var lesstext = "Mostrar menos";       
+    var content = $('#vermas').html();
+
+
+}
+)
